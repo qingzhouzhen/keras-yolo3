f5bc3a64c6948aabc07c7b31c5b0f5389ac63dee
huanghanqing 00416768 <huanghanqing4@huawei.com>
2019-01-15 06:37:51 +0800

    add batch parallel

--- yolo.py
+++ yolo.py
@@ -99,22 +99,26 @@ class YOLO(object):
                 score_threshold=self.score, iou_threshold=self.iou)
         return boxes, scores, classes
 
-    def detect_image(self, image):
+    def detect_image(self, image_array):
+
         start = timer()
+        image_list = []
+        for image in image_array:
+            if self.model_image_size != (None, None):
+                assert self.model_image_size[0]%32 == 0, 'Multiples of 32 required'
+                assert self.model_image_size[1]%32 == 0, 'Multiples of 32 required'
+                boxed_image = letterbox_image(image, tuple(reversed(self.model_image_size)))
+            else:
+                new_image_size = (image.width - (image.width % 32),
+                                  image.height - (image.height % 32))
+                boxed_image = letterbox_image(image, new_image_size)
+            image_data = np.array(boxed_image, dtype='float32')
 
-        if self.model_image_size != (None, None):
-            assert self.model_image_size[0]%32 == 0, 'Multiples of 32 required'
-            assert self.model_image_size[1]%32 == 0, 'Multiples of 32 required'
-            boxed_image = letterbox_image(image, tuple(reversed(self.model_image_size)))
-        else:
-            new_image_size = (image.width - (image.width % 32),
-                              image.height - (image.height % 32))
-            boxed_image = letterbox_image(image, new_image_size)
-        image_data = np.array(boxed_image, dtype='float32')
-
-        print(image_data.shape)
-        image_data /= 255.
-        image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+            # print(image_data.shape)
+            image_data /= 255.
+            image_list.append(image_data)
+        # image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+        image_data = np.stack(image_list, axis=0)
 
         out_boxes, out_scores, out_classes = self.sess.run(
             [self.boxes, self.scores, self.classes],
@@ -124,51 +128,11 @@ class YOLO(object):
                 K.learning_phase(): 0
             })
 
-        print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
-
-        font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
-                    size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
-        thickness = (image.size[0] + image.size[1]) // 300
-
-        for i, c in reversed(list(enumerate(out_classes))):
-            predicted_class = self.class_names[c]
-            box = out_boxes[i]
-            score = out_scores[i]
-
-            label = '{} {:.2f}'.format(predicted_class, score)
-            draw = ImageDraw.Draw(image)
-            label_size = draw.textsize(label, font)
-
-            top, left, bottom, right = box
-            top = max(0, np.floor(top + 0.5).astype('int32'))
-            left = max(0, np.floor(left + 0.5).astype('int32'))
-            bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
-            right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
-            print(label, (left, top), (right, bottom))
-
-            if top - label_size[1] >= 0:
-                text_origin = np.array([left, top - label_size[1]])
-            else:
-                text_origin = np.array([left, top + 1])
-
-            # My kingdom for a good redistributable image drawing library.
-            for i in range(thickness):
-                draw.rectangle(
-                    [left + i, top + i, right - i, bottom - i],
-                    outline=self.colors[c])
-            draw.rectangle(
-                [tuple(text_origin), tuple(text_origin + label_size)],
-                fill=self.colors[c])
-            draw.text(text_origin, label, fill=(0, 0, 0), font=font)
-            del draw
+        # print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
 
         end = timer()
         print(end - start)
-        return image
-
-    def close_session(self):
-        self.sess.close()
-
+        return out_boxes, out_scores, out_classes
 def detect_video(yolo, video_path, output_path=""):
     import cv2
     vid = cv2.VideoCapture(video_path)
